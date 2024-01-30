0527437db3f414716a63a9bdfee55a62ab0eb7f0
smarcon7 <smarcontechnologies7@gmail.com>
2024-01-30 12:43:46 +0530

    approve new asset changes

--- Screens/Sidebar/ApproveNewAsset/ApproveForm.js
+++ Screens/Sidebar/ApproveNewAsset/ApproveForm.js
@@ -1,9 +1,19 @@
-import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import { ScrollView } from 'react-native-gesture-handler';
-import { encode } from 'base-64';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Alert,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ScrollView} from 'react-native-gesture-handler';
+import {encode} from 'base-64';
+import {useNavigation} from '@react-navigation/native';
 
-const ApproveForm = ({ route }) => {
+const ApproveForm = ({route}) => {
+  const navigation = useNavigation();
+
   const [apiData, setApiData] = useState([]);
   const [poNumber, setPoNumber] = useState('');
   const [poDate, setPoDate] = useState('');
@@ -25,17 +35,18 @@ const ApproveForm = ({ route }) => {
   const [center, setCenter] = useState('');
   const [description, setDescription] = useState('');
   const [remarks, setRemarks] = useState('');
-  const [idInv,setIdInv]=useState(0)
-  const [idInvM,setIdInvM]=useState(0)
+  const [idInv, setIdInv] = useState(0);
+  const [idInvM, setIdInvM] = useState(0);
   useEffect(() => {
     const id_inv_m = route.params?.id_inv_m;
-    const id_inv = route.params?.id_inv_m;
+    const id_inv = route.params?.id_inv;
     setIdInvM(id_inv_m);
     setIdInv(id_inv);
     fetchData(id_inv_m, id_inv);
   }, [route.params?.id_inv_m, route.params?.id_inv]);
 
   const fetchData = async (id_inv_m, id_inv) => {
+    console.log(id_inv_m, id_inv, 'oooooooo');
     try {
       const Username = 'SVVG';
       const Password = 'Pass@123';
@@ -46,11 +57,11 @@ const ApproveForm = ({ route }) => {
           headers: {
             Authorization: `Basic ${credentials}`,
           },
-        }
+        },
       );
 
       const responseData = await response.json();
-
+      console.log(responseData, 'resdtssss');
       if (Array.isArray(responseData.data) && responseData.data.length > 0) {
         const itemDetails = responseData.data[0];
         setApiData(itemDetails);
@@ -73,7 +84,7 @@ const ApproveForm = ({ route }) => {
         setLocation(itemDetails.Location);
         setDepartment(itemDetails.Department);
         setCenter(itemDetails.CostCenter);
-        setDescription(itemDetails.st_config);
+        setDescription(itemDetails.ItemDescription);
         setRemarks(itemDetails.Remarks || '');
       } else {
         console.error('Error fetching data: Data is not an array or is empty');
@@ -84,23 +95,39 @@ const ApproveForm = ({ route }) => {
       setApiData([]);
     }
   };
- 
-  const handleAcceptReject = async (status) => {
+
+  const handleAcceptReject = async status => {
     if (!remarks.trim()) {
-      Alert.alert("Alert",'Please enter all the details');
+      Alert.alert('Alert', 'Please enter all the details');
       return;
     }
-  
+
     const currentDate = new Date().toISOString().split('T')[0];
-  
+
+    // Input date in the format dd/mm/yyyy
+    var inputDate = apiData.InvoiceDate;
+
+    // Split the input date into day, month, and year
+    var dateComponents = inputDate.split('/');
+    var day = parseInt(dateComponents[0], 10);
+    var month = parseInt(dateComponents[1], 10);
+    var year = parseInt(dateComponents[2], 10);
+    console.log(day, 'jkikk');
+    // Create a new Date object using the components
+    var formattedDate =
+      year + '-' + ('0' + month).slice(-2) + '-' + ('0' + day).slice(-2);
+    console.log(formattedDate, 'fgtt');
+
+    // Convert the Date object to a string in the format yyyy-mm-dd
+
     const postData = {
       data: [
         {
-          id_emp_user: "1",
+          id_emp_user: '1',
           id_inv: idInv,
           id_inv_m: idInvM,
           dt_approv: currentDate,
-          dt_inv: apiData.InvoiceDate,
+          dt_inv: formattedDate,
           id_loc: apiData.IDLocation,
           id_sgrp: apiData.IDSubCategory,
           id_grp: apiData.IDCategory,
@@ -109,9 +136,9 @@ const ApproveForm = ({ route }) => {
         },
       ],
     };
-  
+
     console.log('postData-->', postData);
-  
+
     try {
       const Username = 'SVVG';
       const Password = 'Pass@123';
@@ -125,23 +152,28 @@ const ApproveForm = ({ route }) => {
             Authorization: `Basic ${credentials}`,
           },
           body: JSON.stringify(postData),
-        }
+        },
       );
-    
+
       // Check if the response is successful (status 2xx)
       if (response.ok) {
+        console.log(response, 'datatatat');
         const responseData = await response.text(); // Get the raw response as text
-    
+        console.log(responseData, 'respppp');
         // Display different alerts based on status
         if (status === 'Accepted') {
-          Alert.alert('Accepted', 'Record has been Accepted successfully');
+          Alert.alert('Accepted', 'Record has been Accepted successfully', [
+            {text: 'OK', onPress: () => navigation.navigate('ApproveNewAsset')},
+          ]);
         } else if (status === 'Rejected') {
-          Alert.alert('Rejected', 'Record has been Rejected successfully');
+          Alert.alert('Rejected', 'Record has been Rejected successfully', [
+            {text: 'OK', onPress: () => navigation.navigate('ApproveNewAsset')},
+          ]);
         } else {
           // Handle other status cases if needed
           Alert.alert('Success', responseData);
         }
-  
+
         // You might want to navigate back or perform other actions here
       } else {
         // Handle error case
@@ -191,190 +223,232 @@ const ApproveForm = ({ route }) => {
   return (
     <ScrollView>
       <View>
-        <View style={{ backgroundColor: '#ff8a3d' }}><Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 18, padding: 10 }}>Item/Model Details</Text></View>
-        <View style={{ marginTop: '5%' }}>
+        <View style={{backgroundColor: '#ff8a3d'}}>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: 'bold',
+              fontSize: 18,
+              padding: 10,
+            }}>
+            Item/Model Details
+          </Text>
+        </View>
+        <View style={{marginTop: '5%'}}>
           <Text style={styles.headings}>Item/Model Name</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setModalName(value)}
+            onChangeText={value => setModalName(value)}
             value={modalName}
             placeholder="Search for item..."
             placeholderTextColor="gray"
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Category</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setCategory(value)}
+            onChangeText={value => setCategory(value)}
             value={category}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Sub Category</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setSubCategory(value)}
+            onChangeText={value => setSubCategory(value)}
             value={subCategory}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Asset Type</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setAssetType(value)}
+            onChangeText={value => setAssetType(value)}
             value={assetType}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Quantity</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setQuantity(value)}
+            onChangeText={value => setQuantity(value)}
             value={quantity}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Unit Price</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setUnitPrice(value)}
+            onChangeText={value => setUnitPrice(value)}
             value={unitPrice}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Location</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setLocation(value)}
+            onChangeText={value => setLocation(value)}
             value={location}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Department</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setDepartment(value)}
+            onChangeText={value => setDepartment(value)}
             value={department}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Cost Center/Project</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setCenter(value)}
+            onChangeText={value => setCenter(value)}
             value={center}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Item Description</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setDescription(value)}
+            onChangeText={value => setDescription(value)}
             value={description}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Remarks</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setRemarks(value)}
+            onChangeText={value => setRemarks(value)}
             value={remarks}
+            multiline={true}
           />
         </View>
-        <View style={{ backgroundColor: '#ff8a3d', marginTop: '3%' }}><Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 18, padding: 10 }}>Invoice Details</Text></View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{backgroundColor: '#ff8a3d', marginTop: '3%'}}>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: 'bold',
+              fontSize: 18,
+              padding: 10,
+            }}>
+            Invoice Details
+          </Text>
+        </View>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>PO Number</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setPoNumber(value)}
+            onChangeText={value => setPoNumber(value)}
             value={poNumber}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>PO Date</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setPoDate(value)}
+            onChangeText={value => setPoDate(value)}
             value={poDate}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Invoice Number</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setInvoiceNumber(value)}
+            onChangeText={value => setInvoiceNumber(value)}
             value={invoiceNumber}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Invoice Date</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setInvoiceDate(value)}
+            onChangeText={value => setInvoiceDate(value)}
             value={invoiceDate}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>GRN Number</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setGrnNumber(value)}
+            onChangeText={value => setGrnNumber(value)}
             value={grnNumber}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>GRN Date</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setGrnDate(value)}
+            onChangeText={value => setGrnDate(value)}
             value={grnDate}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>DC Number</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setDcNumber(value)}
+            onChangeText={value => setDcNumber(value)}
             value={dcNumber}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>DC Date</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setDcDate(value)}
+            onChangeText={value => setDcDate(value)}
             value={dcDate}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ marginTop: '3%' }}>
+        <View style={{marginTop: '3%'}}>
           <Text style={styles.headings}>Vendor</Text>
           <TextInput
             style={styles.textinputs}
-            onChangeText={(value) => setVendor(value)}
+            onChangeText={value => setVendor(value)}
             value={vendor}
             editable={false}
+            multiline={true}
           />
         </View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
           <TouchableOpacity onPress={() => handleAcceptReject('Accepted')}>
             <View style={styles.button}>
               <Text style={styles.buttonText}>Accept</Text>
@@ -389,9 +463,7 @@ const ApproveForm = ({ route }) => {
         </View>
       </View>
     </ScrollView>
-  )
-}
+  );
+};
 
-
-export default ApproveForm
-
+export default ApproveForm;
--- Screens/Sidebar/ApproveNewAsset/ApproveNewAsset.js
+++ Screens/Sidebar/ApproveNewAsset/ApproveNewAsset.js
@@ -1,21 +1,20 @@
-
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Table, Row } from 'react-native-table-component';
-import { encode } from 'base-64';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {Table, Row} from 'react-native-table-component';
+import {encode} from 'base-64';
+import {ScrollView} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Sidebar from '../Sidebar';
-const ApproveNewAsset = ({ navigation }) => {
+const ApproveNewAsset = ({navigation}) => {
   const [apiData, setApiData] = useState([]);
-  
+
   const [sidebarOpen, setSidebarOpen] = useState(false);
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
         <TouchableOpacity
           onPress={handleMenuIconPress}
-          style={{ position: 'absolute', top: '30%', left: '65%', zIndex: 1 }}>
+          style={{position: 'absolute', top: '30%', left: '65%', zIndex: 1}}>
           <Icon name="menu" color="white" size={25} />
         </TouchableOpacity>
       ),
@@ -30,6 +29,7 @@ const ApproveNewAsset = ({ navigation }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 15;
   const tableHeadings = [
+    'S.NO',
     'Invoice No',
     'Invoice Date',
     'Requested By',
@@ -39,19 +39,21 @@ const ApproveNewAsset = ({ navigation }) => {
     'Add to Store',
   ];
 
-  const MyTable = ({ data, headings }) => {
-    const cellWidths = [95, 110, 80, 150, 200, 80, 50];
+  const MyTable = ({data, headings}) => {
+    console.log(data, 'llllllllooooo');
+    const cellWidths = [50, 95, 110, 80, 150, 200, 80, 50];
     const startIdx = (currentPage - 1) * itemsPerPage;
     const endIdx = startIdx + itemsPerPage;
-  
+
     const handleAddToStorePress = (id_inv_m, id_inv) => {
-      navigation.navigate('ApproveForm', { id_inv_m, id_inv });
+      console.log(id_inv_m, id_inv, '6,7');
+      navigation.navigate('ApproveForm', {id_inv_m, id_inv});
     };
-  
+
     return (
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <View style={{ marginTop: '10%', marginBottom: '10%' }}>
-          <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+        <View style={{marginTop: '10%', marginBottom: '10%'}}>
+          <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
             <Row
               data={headings}
               style={{
@@ -66,31 +68,37 @@ const ApproveNewAsset = ({ navigation }) => {
               }}
               widthArr={cellWidths}
             />
+
             {data &&
               data.slice(startIdx, endIdx).map((rowData, rowIndex) => (
-                <Row
-                  key={rowIndex}
-                  data={[
-                    ...rowData.slice(0, 6), // Columns before "Add to Store"
-                    <TouchableOpacity
-                      onPress={() => handleAddToStorePress(rowData[6], rowData[7])} // Pass both id_inv_m and id_inv
-                      key={`plusIcon_${rowIndex}`}
-                      style={{ alignItems: 'center' }}
-                    >
-                      <Icon name="add" size={30} color="#ff8a3d" />
-                    </TouchableOpacity>,
-                  ]}
-                  style={{
-                    height: 35,
-                    justifyContent: 'space-evenly',
-                    color: 'black',
-                  }}
-                  textStyle={{
-                    textAlign: 'center',
-                    color: 'black',
-                  }}
-                  widthArr={[...cellWidths]}
-                />
+                <>
+                  {console.log(rowData[6], rowData[7], '6,7777777777')}
+                  <Row
+                    key={rowIndex}
+                    data={[
+                      rowIndex + 1,
+                      ...rowData.slice(0, 6), // Columns before "Add to Store"
+                      <TouchableOpacity
+                        onPress={() =>
+                          handleAddToStorePress(rowData[6], rowData[7])
+                        } // Pass both id_inv_m and id_inv
+                        key={`plusIcon_${rowIndex}`}
+                        style={{alignItems: 'center'}}>
+                        <Icon name="add" size={30} color="#ff8a3d" />
+                      </TouchableOpacity>,
+                    ]}
+                    style={{
+                      height: 35,
+                      justifyContent: 'space-evenly',
+                      color: 'black',
+                    }}
+                    textStyle={{
+                      textAlign: 'center',
+                      color: 'black',
+                    }}
+                    widthArr={[...cellWidths]}
+                  />
+                </>
               ))}
           </Table>
         </View>
@@ -110,17 +118,17 @@ const ApproveNewAsset = ({ navigation }) => {
           headers: {
             Authorization: `Basic ${credentials}`,
           },
-        }
+        },
       );
 
       const responseData = await response.json();
-
+      console.log(responseData, 'llolllll');
       if (Array.isArray(responseData.data) && responseData.data.length > 0) {
-        const mappedData = responseData.data.map((item) => [
+        const mappedData = responseData.data.map(item => [
           item.InvoiceNo,
           item.InvoiceDate,
           item.RequestBy,
-          item["AssetName/Item"],
+          item['AssetName/Item'],
           item.Vendor,
           item.TotalQty,
           item.id_inv_m,
@@ -129,9 +137,7 @@ const ApproveNewAsset = ({ navigation }) => {
         ]);
         setApiData(mappedData);
       } else {
-        console.error(
-          'Error fetching data: Data is not an array or is empty'
-        );
+        console.error('Error fetching data: Data is not an array or is empty');
         setApiData([]);
       }
     } catch (error) {
@@ -143,7 +149,7 @@ const ApproveNewAsset = ({ navigation }) => {
     fetchData();
   }, []);
 
-  const handlePageChange = (newPage) => {
+  const handlePageChange = newPage => {
     setCurrentPage(newPage);
   };
 
@@ -162,11 +168,11 @@ const ApproveNewAsset = ({ navigation }) => {
           <TouchableOpacity
             style={styles.paginationButton}
             onPress={() => handlePageChange(currentPage - 1)}>
-            <Text style={styles.paginationButtonText}>{"<"}</Text>
+            <Text style={styles.paginationButtonText}>{'<'}</Text>
           </TouchableOpacity>
         )}
 
-        {[...Array(endPage - startPage + 1).keys()].map((index) => (
+        {[...Array(endPage - startPage + 1).keys()].map(index => (
           <TouchableOpacity
             key={startPage + index}
             style={[
@@ -175,9 +181,7 @@ const ApproveNewAsset = ({ navigation }) => {
                 styles.activePaginationButton,
             ]}
             onPress={() => handlePageChange(startPage + index)}>
-            <Text style={styles.paginationButtonText}>
-              {startPage + index}
-            </Text>
+            <Text style={styles.paginationButtonText}>{startPage + index}</Text>
           </TouchableOpacity>
         ))}
 
@@ -185,7 +189,7 @@ const ApproveNewAsset = ({ navigation }) => {
           <TouchableOpacity
             style={styles.paginationButton}
             onPress={() => handlePageChange(currentPage + 1)}>
-            <Text style={styles.paginationButtonText}>{">"}</Text>
+            <Text style={styles.paginationButtonText}>{'>'}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -258,10 +262,10 @@ const ApproveNewAsset = ({ navigation }) => {
         )}
       </View>
       {sidebarOpen && (
-         <View style={styles.sidebar}>
-           <Sidebar isOpen={sidebarOpen} onClose={handleCloseSidebar} />
-         </View>
-       )}
+        <View style={styles.sidebar}>
+          <Sidebar isOpen={sidebarOpen} onClose={handleCloseSidebar} />
+        </View>
+      )}
     </ScrollView>
   );
 };
